41b5476c9df49c2f1437fd83395e9a44d5518b9c
Aniket Patil <patilaniket967@gmail.com>
2024-06-16 08:37:42 +0530

    added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -40,9 +40,9 @@ const Contact = () => {
           {/*form*/}
           <div className="xl:h-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
               <p className="text-white/60">
-                I'll be glad to answer your questions
+                I&apos;ll be glad to answer your questions
               </p>
               {/*input*/}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
